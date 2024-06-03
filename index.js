5ce90b62246aa154c189bdd265dbfaad7c341eb3
Matthew Collier <mcollier@digitalbazaar.com>
2024-06-03 17:46:55 -0400

    Works too.

--- index.js
+++ index.js
@@ -1,20 +1,4 @@
-// import {diag, DiagConsoleLogger, DiagLogLevel} from '@opentelemetry/api';
-// import {ConsoleSpanExporter} from '@opentelemetry/sdk-trace-base';
-// import {NodeSDK} from '@opentelemetry/sdk-node';
-// import {UndiciInstrumentation} from '@opentelemetry/instrumentation-undici';
-
-// // For troubleshooting, set the log level to DiagLogLevel.DEBUG / INFO
-// diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.DEBUG);
-
-// const sdk = new NodeSDK({
-//   traceExporter: new ConsoleSpanExporter(),
-//   instrumentations: [new UndiciInstrumentation()],
-// });
-
-// sdk.start();
-
 import {diag, DiagConsoleLogger, DiagLogLevel} from '@opentelemetry/api'
-
 import { Resource } from '@opentelemetry/resources'
 import { SEMRESATTRS_SERVICE_NAME } from '@opentelemetry/semantic-conventions'
 import { ConsoleSpanExporter, SimpleSpanProcessor } from '@opentelemetry/sdk-trace-base'
@@ -28,7 +12,7 @@ diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.INFO)
 const resource = new Resource({ [SEMRESATTRS_SERVICE_NAME]: 'otel-example' })
 const exporter = new ConsoleSpanExporter()
 const processor = new SimpleSpanProcessor(exporter)
-const tracerProvider = new NodeTracerProvider({ resource })
+const tracerProvider = new NodeTracerProvider()
 
 tracerProvider.addSpanProcessor(processor)
 tracerProvider.register()
