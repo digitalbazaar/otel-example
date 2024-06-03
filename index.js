b8e6a4d411ae820cac94c26c978de3046114a6bb
Matthew Collier <mcollier@digitalbazaar.com>
2024-06-03 17:35:18 -0400

    Works.

--- index.js
+++ index.js
@@ -1,17 +1,39 @@
-import {diag, DiagConsoleLogger, DiagLogLevel} from '@opentelemetry/api';
-import {ConsoleSpanExporter} from '@opentelemetry/sdk-trace-base';
-import {NodeSDK} from '@opentelemetry/sdk-node';
-import {UndiciInstrumentation} from '@opentelemetry/instrumentation-undici';
+// import {diag, DiagConsoleLogger, DiagLogLevel} from '@opentelemetry/api';
+// import {ConsoleSpanExporter} from '@opentelemetry/sdk-trace-base';
+// import {NodeSDK} from '@opentelemetry/sdk-node';
+// import {UndiciInstrumentation} from '@opentelemetry/instrumentation-undici';
+
+// // For troubleshooting, set the log level to DiagLogLevel.DEBUG / INFO
+// diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.DEBUG);
+
+// const sdk = new NodeSDK({
+//   traceExporter: new ConsoleSpanExporter(),
+//   instrumentations: [new UndiciInstrumentation()],
+// });
+
+// sdk.start();
+
+import {diag, DiagConsoleLogger, DiagLogLevel} from '@opentelemetry/api'
+
+import { Resource } from '@opentelemetry/resources'
+import { SEMRESATTRS_SERVICE_NAME } from '@opentelemetry/semantic-conventions'
+import { ConsoleSpanExporter, SimpleSpanProcessor } from '@opentelemetry/sdk-trace-base'
+import { NodeTracerProvider } from '@opentelemetry/sdk-trace-node'
+import { registerInstrumentations } from '@opentelemetry/instrumentation'
+import { UndiciInstrumentation } from '@opentelemetry/instrumentation-undici'
 
 // For troubleshooting, set the log level to DiagLogLevel.DEBUG / INFO
-diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.DEBUG);
+diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.INFO)
 
-const sdk = new NodeSDK({
-  traceExporter: new ConsoleSpanExporter(),
-  instrumentations: [new UndiciInstrumentation()],
-});
+const resource = new Resource({ [SEMRESATTRS_SERVICE_NAME]: 'otel-example' })
+const exporter = new ConsoleSpanExporter()
+const processor = new SimpleSpanProcessor(exporter)
+const tracerProvider = new NodeTracerProvider({ resource })
 
-sdk.start();
+tracerProvider.addSpanProcessor(processor)
+tracerProvider.register()
+
+registerInstrumentations({ instrumentations: [new UndiciInstrumentation()] })
 
 const response = await fetch('https://github.com/');
 const body = await response.text();
