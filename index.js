c44096cbb79abd03a5e32d6d249feeab6ab67677
Matthew Collier <mcollier@digitalbazaar.com>
2024-06-03 19:20:19 -0400

    Working NodeSDK.

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import {diag, DiagConsoleLogger, DiagLogLevel} from '@opentelemetry/api'
 import { Resource } from '@opentelemetry/resources'
 import { SEMRESATTRS_SERVICE_NAME } from '@opentelemetry/semantic-conventions'
 import { ConsoleSpanExporter, SimpleSpanProcessor } from '@opentelemetry/sdk-trace-base'
+import { NodeSDK } from '@opentelemetry/sdk-node'
 import { NodeTracerProvider } from '@opentelemetry/sdk-trace-node'
 import { registerInstrumentations } from '@opentelemetry/instrumentation'
 import { UndiciInstrumentation } from '@opentelemetry/instrumentation-undici'
@@ -12,12 +13,15 @@ diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.INFO)
 const resource = new Resource({ [SEMRESATTRS_SERVICE_NAME]: 'otel-example' })
 const exporter = new ConsoleSpanExporter()
 const processor = new SimpleSpanProcessor(exporter)
-const tracerProvider = new NodeTracerProvider()
 
-tracerProvider.addSpanProcessor(processor)
-tracerProvider.register()
+const sdk = new NodeSDK({
+  resource,
+  autoDetectResources: false,
+  spanProcessors: [processor],
+  instrumentations: [new UndiciInstrumentation()],
+})
 
-registerInstrumentations({ instrumentations: [new UndiciInstrumentation()] })
+sdk.start()
 
 const response = await fetch('https://github.com/');
 const body = await response.text();
